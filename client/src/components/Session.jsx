49b589a34388c2ed6af742db2691acc1d9731c4b
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-08-26 11:25:34 -0700

    Removed unnecessary renderPose method. Fixed index calculation. Displaying practice ended message.

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -6,31 +6,15 @@ class Session extends React.Component {
     super(props);
     this.state = {
       index: 0,
-      currentPose: this.props.selectedPoses[0]
+      currentPose: this.props.selectedPoses[0],
+      ended: false,
     }
     this.onComplete = this.onComplete.bind(this);
-    this.renderPose = this.renderPose.bind(this);
-    this.endMsg = this.endMsg.bind(this);
-  }
-
-  renderPose() {
-    return (
-      <div>
-        {this.state.currentPose && <img src={this.state.currentPose.picture} />}
-        {this.state.currentPose && <Countdown key={this.state.index} date={Date.now() + 3000} onComplete={this.onComplete} />}
-      </div>
-    );
-  }
-
-  endMsg() {
-    return (
-      <span>Your practice has ended.</span>
-    )
   }
 
   onComplete({ minutes, seconds, completed }) {
     if (completed) {
-      if (this.state.index < this.props.selectedPoses.length) {
+      if (this.state.index < this.props.selectedPoses.length - 1) {
         this.setState({
           index: this.state.index + 1,
         }, ()=> {
@@ -39,7 +23,9 @@ class Session extends React.Component {
           })
         })
       } else {
-        endMsg();
+        this.setState({
+          ended: true,
+        })
       }
     } else {
       return <span className="countdown">{minutes}:{seconds}</span>
@@ -47,10 +33,16 @@ class Session extends React.Component {
   }
 
   render() {
-    console.log(`rendering`)
-    console.log('index:', this.state.index)
-    console.log('pose id:', this.state.currentPose && this.state.currentPose.id)
-    return this.renderPose();
+    return (
+      <>
+      {this.state.ended ? <span id="end">Your practice has endeded.</span>
+      :
+      (<div>
+        {this.state.currentPose && <img src={this.state.currentPose.picture} />}
+        {this.state.currentPose && <Countdown key={this.state.index} date={Date.now() + 3000} onComplete={this.onComplete} />}
+      </div>)}
+      </>
+    );
   }
 }
 
